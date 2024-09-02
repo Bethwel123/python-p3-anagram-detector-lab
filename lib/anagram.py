fd6b4fdd70da3ec02b1f3ebd214b4319e854bb6d
Bethwel123 <bethwellangat654@gmail.com>
2024-09-02 14:12:17 +0300

    Added code to anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, word_list):
+        matches = []
+        for enlist_word in word_list:
+            if sorted(enlist_word) == self.sorted_word:
+                matches.append(enlist_word)
+        return matches
